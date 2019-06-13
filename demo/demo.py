76f3a5ef553ea15746e83ba51e8e6c54b26ee771
danmalowany <35199527+danmalowany@users.noreply.github.com>
2019-06-13 12:39:15 +0300

    Create demo.py

---
+++ demo/demo.py
@@ -0,0 +1,67 @@
+
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+
+import requests
+from io import BytesIO
+from PIL import Image
+import numpy as np
+# this makes our figures bigger
+pylab.rcParams['figure.figsize'] = 20, 12
+from maskrcnn_benchmark.config import cfg
+from predictor import COCODemo
+
+from trains import Task
+task = Task.init()
+
+
+config_file = "../configs/caffe2/e2e_mask_rcnn_R_50_FPN_1x_caffe2.yaml"
+
+# update the config options with the config file
+cfg.merge_from_file(config_file)
+# manual override some options
+cfg.merge_from_list(["MODEL.DEVICE", "cpu"])
+coco_demo = COCODemo(
+    cfg,
+    min_image_size=800,
+    confidence_threshold=0.7,
+)
+
+
+def load(url):
+    """
+    Given an url of an image, downloads the image and
+    returns a PIL image
+    """
+    response = requests.get(url)
+    pil_image = Image.open(BytesIO(response.content)).convert("RGB")
+    # convert to BGR format
+    image = np.array(pil_image)[:, :, [2, 1, 0]]
+    return image
+
+
+def imshow(img):
+    plt.imshow(img[:, :, [2, 1, 0]])
+    plt.axis("off")
+    plt.show()
+
+
+image = load("http://farm3.staticflickr.com/2469/3915380994_2e611b1779_z.jpg")
+imshow(image)
+# compute predictions
+predictions = coco_demo.run_on_opencv_image(image)
+imshow(predictions)
+# set up demo for keypoints
+config_file = "../configs/caffe2/e2e_keypoint_rcnn_R_50_FPN_1x_caffe2.yaml"
+cfg.merge_from_file(config_file)
+cfg.merge_from_list(["MODEL.DEVICE", "cpu"])
+cfg.merge_from_list(["MODEL.MASK_ON", False])
+
+coco_demo = COCODemo(
+    cfg,
+    min_image_size=800,
+    confidence_threshold=0.7,
+)
+image = load("http://farm9.staticflickr.com/8419/8710147224_ff637cc4fc_z.jpg")
+predictions = coco_demo.run_on_opencv_image(image)
+imshow(predictions)
